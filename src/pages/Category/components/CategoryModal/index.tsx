45d504d229c434d9fcbcbb40f50e6c6b827adc01
Anna Wagner <annwagner8888@gmail.com>
2024-01-23 15:28:40 +0200

    FAK-24: Add category modal

---
+++ src/pages/Category/components/CategoryModal/index.tsx
@@ -0,0 +1,78 @@
+import { Form } from 'antd';
+import {
+  FC,
+  memo
+} from 'react';
+
+import {
+  BasicModal,
+  Input
+} from '../../../../components';
+import { layout } from '../../../../constants/form';
+import { ICategory } from '../../../../types';
+import { replaceSpaceToUnderscore } from '../../../../utils/string';
+
+interface ICategoryModalProps {
+  isOpen: boolean;
+  category: ICategory | null;
+  onCreate: (category: ICategory) => void;
+  onCancel: () => void
+}
+
+const CategoryModal: FC<ICategoryModalProps> = ({ isOpen, category, onCreate, onCancel }) => {
+  const title: string = category ? 'Edit category' : 'Add new category';
+  const initialValues = category || {name: ''};
+  const [ form ] = Form.useForm();
+  
+  const onCloseModal = (): void => {
+    form.resetFields();
+    onCancel();
+  };
+  
+  const onFinish = (values: ICategory): void => {
+    onCreate({
+      ...initialValues,
+      ...values,
+      name: replaceSpaceToUnderscore(values.name).toLowerCase()
+    })
+    onCloseModal();
+  };
+
+  return (
+    <BasicModal
+      title={title}
+      isOpen={isOpen}
+      okText="Save"
+      onOk={form.submit}
+      onCancel={onCloseModal}
+    >
+      <Form
+        form={form}
+        {...layout}
+        initialValues={initialValues}
+        name="medicine-modal"
+        autoComplete="off"
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="Name"
+          name="name"
+          data-testid="name"
+          rules={[
+            { required: true, message: 'Please input name!' },
+            {
+              type: 'string',
+              min: 2,
+              max: 50,
+              message: 'Recipient must be from 2 characters to 50 characters!'
+            }
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </BasicModal>
+  );
+};
+
+export default memo(CategoryModal);
