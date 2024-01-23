4b77bc100ed055512f51a948b911cf1ef8bcec7f
Anna Wagner <annwagner8888@gmail.com>
2024-01-23 15:20:57 +0200

    FAK-19: Add medicine modal

---
+++ src/pages/Home/components/MedicineModal/index.tsx
@@ -0,0 +1,127 @@
+import { Form } from 'antd';
+import {
+  FC,
+  memo
+} from 'react';
+
+import {
+  BasicModal,
+  Input,
+  Select
+} from '../../../../components';
+import { layout } from '../../../../constants/form';
+import { useFetchCategoryQuery } from '../../../../services/category';
+import { Medicine } from '../../../../types/medicine';
+import { generateOptions } from '../../../../utils/form';
+
+import {
+  createInitFormValues,
+  generateMedicine,
+  IFormValues
+} from '../helpers';
+
+interface IMedicineModalProps {
+  isOpen: boolean;
+  medicine: Medicine | null;
+  onCreate: (medicine: Medicine) => void;
+  onCancel: () => void
+}
+
+const MedicineModal: FC<IMedicineModalProps> = ({ isOpen, medicine, onCreate, onCancel }) => {
+  const { data: categories = [] } = useFetchCategoryQuery({});
+  const initialValues = createInitFormValues(medicine);
+  const [ form ] = Form.useForm();
+  
+  console.log('medicineNames');
+  
+  const onCloseModal = (): void => {
+    form.resetFields();
+    onCancel();
+  };
+  
+  const onFinish = (values: IFormValues): void => {
+    onCreate(generateMedicine(values, medicine, categories));
+    onCloseModal();
+  };
+
+  return (
+    <BasicModal
+      title={medicine?.id ? 'Edit medicine' : 'Add new medicine'}
+      isOpen={isOpen}
+      okText="Save"
+      onOk={form.submit}
+      onCancel={onCloseModal}
+    >
+      <Form
+        form={form}
+        {...layout}
+        initialValues={initialValues}
+        name="medicine-modal"
+        autoComplete="off"
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="categoryNames"
+          label="Category"
+          rules={[
+            { required: true, message: 'Please select category!' }
+          ]}
+        >
+          <Select
+            mode="multiple"
+            placeholder="Select category"
+            allowClear
+            options={generateOptions(categories, 'name')}
+          />
+        </Form.Item>
+        <Form.Item
+          label="Name"
+          name="name"
+          data-testid="name"
+          rules={[
+            { required: true, message: 'Please input name!' },
+            {
+              type: 'string',
+              min: 2,
+              max: 50,
+              message: 'Recipient must be from 2 characters to 50 characters!'
+            }
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Amount"
+          name="amount"
+          rules={[
+            { required: true, message: 'Please input Amount!' },
+            {
+              type: 'number',
+              min: 1,
+              message: 'Amount must be minimum 1 characters!'
+            }
+          ]}
+        >
+          <Input type="number" min={1} data-testid="modal-medicine-amount" />
+        </Form.Item>
+        <Form.Item
+          label="Expiration date"
+          name="expiration_date"
+          data-testid="expiration_date"
+          rules={[ { required: true, message: 'Please input date!' } ]}
+        >
+          <Input type="datepicker" />
+        </Form.Item>
+        <Form.Item
+          label="Description"
+          name="description"
+          data-testid="description"
+        >
+          <Input type="textarea" rows={4} />
+        </Form.Item>
+      </Form>
+    </BasicModal>
+  );
+};
+
+export default memo(MedicineModal);
