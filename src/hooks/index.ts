2b87f2cc47ed48f8be4a8910d5a6445da06643fa
Anna Wagner <annwagner8888@gmail.com>
2024-01-23 15:13:55 +0200

    FAK-16: Add index file for hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,17 @@
+import { useAuth } from './useAuth';
+import { useModal } from './useModal';
+import { useNotification } from './useNotification';
+import { useColumnSearch } from './useColumnSearch';
+import { useColumn } from './useColumn';
+import { useQueryFilters } from './useQueryFilters';
+import { useTabs } from './useTabs';
+
+export {
+  useModal,
+  useNotification,
+  useAuth,
+  useColumn,
+  useColumnSearch,
+  useQueryFilters,
+  useTabs
+};
